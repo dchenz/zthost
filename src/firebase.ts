b50c4b747cbb3d4b7f909caebe98abd1380742ad
Danny <51312422+dchenz@users.noreply.github.com>
2023-09-03 05:47:08 +0000

    Move firebase config to env and commit security rules
    - Current config is for testing and will be deleted eventually
    - Firestore security rules should go with the app code
    - Setup local deploy using netlify cli

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, inMemoryPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_NnInIsJNlZPwvTXexBS9dkl47p3nAes",
-  authDomain: "zthost-firebase.firebaseapp.com",
-  projectId: "zthost-firebase",
-  storageBucket: "zthost-firebase.appspot.com",
-  messagingSenderId: "203474789030",
-  appId: "1:203474789030:web:022d2e12925531854b8a67",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
