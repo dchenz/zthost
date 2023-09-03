61a97e92127fcfecab0066c1376feb88ce7f8f09
Danny <51312422+dchenz@users.noreply.github.com>
2023-09-04 01:16:47 +1000

    Fix styling of password form inputs

--- src/components/PasswordLogin/CreatePasswordForm.tsx
+++ src/components/PasswordLogin/CreatePasswordForm.tsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Button, FormControl, Input, Text, VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 const MINIMUM_LENGTH = 8;
@@ -34,29 +27,23 @@ const CreatePasswordForm: React.FC<CreatePasswordFormProps> = ({
     <form onSubmit={onFormSubmit} style={{ width: "400px" }}>
       <VStack gap={5}>
         <FormControl>
-          <FormLabel>Password</FormLabel>
           <Input
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter new password"
           />
           <Text color="grey" fontSize="14px">
             Must be at least {MINIMUM_LENGTH} characters.
           </Text>
         </FormControl>
         <FormControl>
-          <FormLabel>Confirm password</FormLabel>
           <Input
             type="password"
             onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm new password"
           />
         </FormControl>
-        <Button
-          colorScheme="teal"
-          type="submit"
-          variant="solid"
-          isDisabled={!canSubmit}
-          width="100%"
-        >
+        <Button type="submit" isDisabled={!canSubmit} width="100%">
           Create
         </Button>
       </VStack>
