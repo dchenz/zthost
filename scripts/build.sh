c576827a9ed78b4cf32336f78252329100ffaf57
Danny <51312422+dchenz@users.noreply.github.com>
2023-10-03 00:50:15 +1100

    Move git checkout into build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+if [ -n "$1" ]; then
+    git -c advice.detachedHead=false checkout $1 || exit 1
+fi
+
 export GENERATE_SOURCEMAP=false
 export REACT_APP_GIT_COMMIT_HASH=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 react-scripts build
+
+if [ -n "$1" ]; then
+    git checkout main
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then
-    git -c advice.detachedHead=false checkout $1 || exit 1
-fi
-
-sh scripts/build.sh
+sh scripts/build.sh $1
 
 echo "/* /index.html 200" > build/_redirects
 
 netlify deploy --dir build --prod
-
-if [ -n "$1" ]; then
-    git checkout main
-fi
-
