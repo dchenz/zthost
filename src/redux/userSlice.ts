a2837f34260a834f13c6ca6bf866d5dc208e5e64
Danny <51312422+dchenz@users.noreply.github.com>
2024-01-13 21:14:00 +1100

    Move login/logout functions into user slice

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,4 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../config";
+import { auth } from "../firebase";
+import { useChakraToast } from "../utils";
 import type { AuthProperties, User } from "../database/model";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
@@ -31,3 +38,47 @@ export const userSlice = createSlice({
 });
 
 export const { setUser, setUserAuth, setAccessToken } = userSlice.actions;
+
+export const useLogout = () => {
+  const dispatch = useDispatch();
+
+  return useCallback(async () => {
+    await auth.signOut();
+    dispatch(setAccessToken(null));
+    dispatch(setUserAuth(null));
+    dispatch(setUser(null));
+  }, []);
+};
+
+export const useLogin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { openToast, updateToast, closeToast } = useChakraToast();
+
+  return useCallback(async () => {
+    openToast({
+      title: "Waiting for confirmation.",
+      status: "info",
+      duration: null,
+    });
+    try {
+      const provider = new GoogleAuthProvider();
+      provider.addScope("https://www.googleapis.com/auth/drive.file");
+      const response = await signInWithPopup(auth, provider);
+      if (response.user) {
+        closeToast();
+        navigate(ROUTES.loginWithPassword);
+      }
+      const credentials = GoogleAuthProvider.credentialFromResult(response);
+      if (credentials?.accessToken) {
+        dispatch(setAccessToken(credentials.accessToken));
+      }
+    } catch (e) {
+      updateToast({
+        title: "Unable to sign in with Google. Please try again.",
+        status: "error",
+        duration: 3000,
+      });
+    }
+  }, []);
+};
