1fcb867742559c8ea55a98a15d91a2824dbfe5dc
Danny <51312422+dchenz@users.noreply.github.com>
2024-01-14 23:02:47 +1100

    Implement uploading a single chunk

--- src/redux/blobApi.ts
+++ src/redux/blobApi.ts
@@ -1,12 +1,44 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import { encrypt, generateWrappedKey } from "../utils/crypto";
+import type {
+  AuthProperties,
+  BlobStorage,
+  FileChunkKey,
+} from "../database/model";
+
+type UserState = {
+  user: { storage: BlobStorage | null; userAuth: AuthProperties | null };
+};
+
+type CreateChunkQuery = {
+  blob: Blob;
+  onProgress: (n: number) => void;
+};
 
 export const blobApi = createApi({
   reducerPath: "blobApi",
   baseQuery: fakeBaseQuery(),
   endpoints: (builder) => ({
-    createChunk: builder.mutation<void, void>({
-      queryFn: async () => {
-        return { data: undefined };
+    createChunk: builder.mutation<FileChunkKey, CreateChunkQuery>({
+      queryFn: async ({ blob, onProgress }, api) => {
+        const state = api.getState() as UserState;
+        const { plainTextKey, wrappedKey } = await generateWrappedKey(
+          state.user.userAuth!.fileKey
+        );
+        const encryptedChunk = await encrypt(
+          await blob.arrayBuffer(),
+          plainTextKey
+        );
+        const blobId = await state.user.storage!.putBlob(
+          encryptedChunk,
+          onProgress
+        );
+        return {
+          data: {
+            id: blobId,
+            key: Buffer.from(wrappedKey).toString("base64"),
+          },
+        };
       },
     }),
 
